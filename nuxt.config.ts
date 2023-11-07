092666beec5569122b1d8166aa52d09e2443d695
Teshane Crawford <crawfordteshane@gmail.com>
2023-11-07 15:23:26 -0500

    :construction: Add update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  typescript: {
+    strict: true,
+    shim: false
+  },
   modules: [
     '@nuxt/content',
     '@nuxthq/studio'
   ],
   content: {
     // ... options
-  }
+  },
+  css: [
+    '~/assets/css/main.css',
+    '@unocss/reset/tailwind.css'
+  ],
 })
