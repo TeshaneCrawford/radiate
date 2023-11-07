def5addaf2670e1f62221981ea2fbf80710df950
Teshane Crawford <crawfordteshane@gmail.com>
2023-11-07 13:17:05 -0500

    :construction: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxt/content'
+    '@nuxt/content',
+    '@nuxthq/studio'
   ],
   content: {
     // ... options
